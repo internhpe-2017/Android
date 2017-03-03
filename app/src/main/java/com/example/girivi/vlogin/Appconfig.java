898b289306d95d6d24b1b15d7de4cfc509374740
vidyashankar93 <root@localhost.localdomain>
2017-03-03 17:13:45 +0530

    This is android +Gradle

---
+++ app/src/main/java/com/example/girivi/vlogin/Appconfig.java
@@ -0,0 +1,10 @@
+package com.example.girivi.vlogin;
+
+
+
+public class Appconfig {
+
+    public static final String LOGIN_URL = "http://192.168.43.40:1080/source/include/dblogin.php";
+    public static final String REGISTER_URL = "http://192.168.43.40:1080/source/include/dbregister.php";
+
+}
