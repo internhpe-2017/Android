4ae806e95299fbafad3d6ea7e866b2ccc7601af9
narendrahm <narendrahmn@gmail.com>
2017-03-24 20:50:23 +0530

    Detailed Report

---
+++ app/src/main/java/com/example/girivi/vlogin/DetailedReport.java
@@ -0,0 +1,70 @@
+package com.example.girivi.vlogin;
+
+import android.app.ProgressDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+public class DetailedReport extends AppCompatActivity implements View.OnClickListener{
+
+    public static final String JSON_URL = "http://www.triphpe.890m.com/getData.php";
+
+    private Button buttonGet;
+
+    private ListView listView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detailed_report);
+
+        buttonGet = (Button) findViewById(R.id.buttonGet);
+        buttonGet.setOnClickListener(this);
+        listView = (ListView) findViewById(R.id.listView);
+    }
+
+    private void sendRequest(){
+        final ProgressDialog loading = ProgressDialog.show(this,"Loading Data", "Please wait...",false,false);
+        StringRequest stringRequest = new StringRequest(JSON_URL,
+                new Response.Listener<String>() {
+                    @Override
+
+                    public void onResponse(String response) {
+                        showJSON(response);
+                        loading.dismiss();
+
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Toast.makeText(DetailedReport.this,error.getMessage(),Toast.LENGTH_LONG).show();
+                        loading.dismiss();
+                    }
+                });
+
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        requestQueue.add(stringRequest);
+    }
+
+    private void showJSON(String json){
+        ParseJSON pj = new ParseJSON(json);
+        pj.parseJSON();
+        CustomList cl = new CustomList(this, ParseJSON.ids,ParseJSON.occasion,ParseJSON.particulars,ParseJSON.amount,ParseJSON.date,ParseJSON.to);
+        listView.setAdapter(cl);
+    }
+
+    @Override
+    public void onClick(View v) {
+        sendRequest();
+    }
+}
---
+++ app/src/main/java/com/example/girivi/vlogin/ParseJSON.java
@@ -0,0 +1,65 @@
+package com.example.girivi.vlogin;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+/**
+ * Created by hmna on 3/24/2017.
+ */
+
+public class ParseJSON {
+    public static String[] ids;
+    public static String[] occasion;
+    public static String[] particulars;
+    public static String[] amount;
+    public static String[] date;
+    public static String[] to;
+
+    public static final String JSON_ARRAY = "result";
+    public static final String KEY_ID = "id";
+    public static final String KEY_OCCASION = "occasion";
+    public static final String KEY_PARTICULARS = "particulars";
+    public static final String KEY_AMOUNT = "amount";
+    public static final String KEY_DATE = "date";
+    public static final String KEY_TO = "to";
+
+    private JSONArray users = null;
+    int qtySum=0;
+    int qtyNum;
+    private String json;
+
+    public ParseJSON(String json){
+        this.json = json;
+    }
+
+    protected void parseJSON(){
+        JSONObject jsonObject=null;
+        try {
+            jsonObject = new JSONObject(json);
+            users = jsonObject.getJSONArray(JSON_ARRAY);
+
+            ids = new String[users.length()];
+            occasion = new String[users.length()];
+            particulars = new String[users.length()];
+            amount = new String[users.length()];
+            date = new String[users.length()];
+            to = new String[users.length()];
+
+            for(int i=0;i<users.length();i++){
+                JSONObject jo = users.getJSONObject(i);
+                ids[i] = jo.getString(KEY_ID);
+                occasion[i] = jo.getString(KEY_OCCASION);
+                particulars[i] = jo.getString(KEY_PARTICULARS);
+                amount[i] = jo.getString(KEY_AMOUNT);
+                date[i] = jo.getString(KEY_DATE);
+                to[i] = jo.getString(KEY_TO);
+                qtyNum = Integer.parseInt(jo.getString(KEY_AMOUNT));
+                qtySum += qtyNum;
+            }
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+}
