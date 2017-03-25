06c7e39966468a384cbc6b02e60c421837aed939
KeerthiAnbazhagan <keerthukrishna70@gmail.com>
2017-03-25 20:25:17 +0530

    Update Appconfig.java

--- app/src/main/java/com/example/girivi/vlogin/Appconfig.java
+++ app/src/main/java/com/example/girivi/vlogin/Appconfig.java
@@ -7,4 +7,6 @@ public class Appconfig {
     public static final String REGISTER_URL = "http://triphpe.16mb.com/vreg.php";
     public static final String Forgot_URL = "http://triphpe.16mb.com/forgotpass.php";
     public static final String Reset_URL= "http://triphpe.16mb.com/reset.php";
+    public static final String ADD_URL = "http://triphpe.16mb.com/expense.php";
+    public static final String SETTLEMENT_URL = "http://triphpe.16mb.com/settlement.php";
 }
